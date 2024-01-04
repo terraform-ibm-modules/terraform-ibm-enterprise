c3dbf035c5375b39b3fc4f3637b7b366d46ed9ee
ocofaigh <ocofaigh@ie.ibm.com>
2024-01-04 17:48:11 +0000

    fix pre-commit hook failure

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -11,20 +11,20 @@ module "enterprise" {
   enterprise_json_input = {
     account_groups = [{
       # Here is how to create a hierarchy of enterprise child accounts and account groups
-      # NB: There is currently a limitation with this when doing terraform destroy as accounts do not get fully deleted 
-      # until 21 days after deletion process has been triggered, and so the destroy of the account group will fail 
+      # NB: There is currently a limitation with this when doing terraform destroy as accounts do not get fully deleted
+      # until 21 days after deletion process has been triggered, and so the destroy of the account group will fail
       # until after the 21 days are up. A force delete option is being considered by backend team to address this.
       # This is why the account creation is commented out below.
 
-      name="${var.prefix}_account_group_depth_0",
+      name = "${var.prefix}_account_group_depth_0",
       # accounts = [{ name = "${var.prefix}_account_depth_0" }],
-      account_groups=[
+      account_groups = [
         {
-          name="${var.prefix}_account_group_depth_1",
+          name = "${var.prefix}_account_group_depth_1",
           # accounts = [{ name = "${var.prefix}_account_depth_1" }],
-          account_groups=[
+          account_groups = [
             {
-              name="${var.prefix}_account_group_depth_2",
+              name = "${var.prefix}_account_group_depth_2",
               # accounts = [{ name = "${var.prefix}_account_depth_2" }],
             }
           ]
