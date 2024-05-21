b9c8d018d27cf8ac7428bc76967a6adcbb475954
namrata <namrta.bhadauriya@ibm.com>
2024-05-21 20:41:53 +0530

    iam_api_key creation in sub accounts

--- submodules/dynamic_values/variables.tf
+++ submodules/dynamic_values/variables.tf
@@ -2,7 +2,8 @@
 
 variable "enterprise_accounts" {
   description = "List of enterprise child accounts in the enterprise"
-  type        = list(object({ name = string, key_name = string, parent_key_name = optional(string, null), owner_iam_id = optional(string, null) }))
+  type        = list(object({ name = string, key_name = string, parent_key_name = optional(string, null), owner_iam_id = optional(string, null), add_owner_iam_policies = optional(bool, false) }))
+  default     = []
   validation {
     error_message = "Accounts key_name should be unique"
     condition     = length(distinct(var.enterprise_accounts[*].key_name)) == length(var.enterprise_accounts[*].key_name)
@@ -12,6 +13,7 @@ variable "enterprise_accounts" {
 variable "enterprise_account_groups" {
   description = "List of enterprise child account_groups in the enterprise"
   type        = list(object({ name = string, key_name = string, parent_key_name = optional(string, null), owner_iam_id = optional(string, null) }))
+  default     = []
   validation {
     error_message = "Account_Groups key_name should be unique"
     condition     = length(distinct(var.enterprise_account_groups[*].key_name)) == length(var.enterprise_account_groups[*].key_name)
--- variables.tf
+++ variables.tf
@@ -15,7 +15,8 @@ variable "enterprise_primary_contact_iam_id" {
 
 variable "enterprise_accounts" {
   description = "List of enterprise child accounts in the enterprise"
-  type        = list(object({ name = string, key_name = string, parent_key_name = optional(string, null), owner_iam_id = optional(string, null) }))
+  type        = list(object({ name = string, key_name = string, parent_key_name = optional(string, null), owner_iam_id = optional(string, null), add_owner_iam_policies = optional(bool, false) }))
+  default     = []
   validation {
     error_message = "Accounts key_name should be unique"
     condition     = length(distinct(var.enterprise_accounts[*].key_name)) == length(var.enterprise_accounts[*].key_name)
@@ -25,6 +26,7 @@ variable "enterprise_accounts" {
 variable "enterprise_account_groups" {
   description = "List of enterprise child account_groups in the enterprise"
   type        = list(object({ name = string, key_name = string, parent_key_name = optional(string, null), owner_iam_id = optional(string, null) }))
+  default     = []
   validation {
     error_message = "Account_Groups key_name should be unique"
     condition     = length(distinct(var.enterprise_account_groups[*].key_name)) == length(var.enterprise_account_groups[*].key_name)
