52b6bef99b55a28f71a48752af0f445fd6e03ecf
namrata <namrta.bhadauriya@ibm.com>
2024-08-29 19:17:46 +0530

    added extra fieds for pr_test.go

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,15 +2,32 @@
 package test
 
 import (
+	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
 
 const basicExampleDir = "examples/basic"
 const advancedExampleDir = "examples/advanced"
-const completeExampleDir = "examples/complete"
+const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-resources.yaml"
+
+var permanentResources map[string]interface{}
+
+// TestMain will be run before any parallel tests, used to read data from yaml for use with tests
+func TestMain(m *testing.M) {
+
+	var err error
+	permanentResources, err = common.LoadMapFromYaml(yamlLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+}
 
 func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
@@ -22,6 +39,23 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 	options.TerraformVars = map[string]interface{}{
 		"prefix":          options.Prefix,
 		"enterprise_name": "DAF Enterprise",
+	}
+
+	return options
+}
+
+func setupAdvancedOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
+	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+		Testing:      t,
+		TerraformDir: dir,
+		Prefix:       prefix,
+	})
+
+	options.TerraformVars = map[string]interface{}{
+		"prefix":                      options.Prefix,
+		"enterprise_name":             "DAF Enterprise",
+		"existing_sm_instance_guid":   permanentResources["secretsManagerGuid"],
+		"existing_sm_instance_region": permanentResources["secretsManagerRegion"],
 	}
 
 	return options
@@ -40,7 +74,7 @@ func TestRunBasicExample(t *testing.T) {
 func TestRunAdvancedExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "enterprise-com", advancedExampleDir)
+	options := setupAdvancedOptions(t, "enterprise-com", advancedExampleDir)
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
@@ -50,21 +84,11 @@ func TestRunAdvancedExample(t *testing.T) {
 func TestRunUpgradeAdvancedExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "enterprise-upg", advancedExampleDir)
+	options := setupAdvancedOptions(t, "enterprise-upg", advancedExampleDir)
 
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
 		assert.Nil(t, err, "This should not have errored")
 		assert.NotNil(t, output, "Expected some output")
 	}
-}
-
-func TestRunCompleteExample(t *testing.T) {
-	t.Parallel()
-
-	options := setupOptions(t, "enterprise-com", completeExampleDir)
-
-	output, err := options.RunTestConsistency()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
 }
