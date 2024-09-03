bca2c4c8c2c728c72e4131a82c1ad2693129c700
namratabhadauriya <namrata.nitw@gmail.com>
2024-09-03 15:43:15 +0530

    feat: added support to set boolean `add_owner_iam_policies` inside the `enterprise_accounts` input. Setting this to true will create an IAM service ID in the new account and will also create an apikey. The apikey value can be retrieved using the new `enterprise_accounts_iam_response` output with the following syntax: `module.enterprise.enterprise_accounts_iam_response[0].iam_apikey` (#182)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -21,9 +21,10 @@ module "enterprise" {
   }]
   enterprise_accounts = [
     {
-      key_name        = "${var.prefix}-acc-key-1"
-      name            = "${var.prefix}_account_1"
-      parent_key_name = null
+      key_name               = "${var.prefix}-acc-key-1"
+      name                   = "${var.prefix}_account_1"
+      parent_key_name        = null
+      add_owner_iam_policies = true # this field enable child account to have IAM_APIKey with owner IAM policies
     },
     {
       key_name        = "${var.prefix}-acc-key-2"
@@ -31,4 +32,67 @@ module "enterprise" {
       parent_key_name = null
     }
   ]
+}
+
+########################################################################################################################
+# Locals
+########################################################################################################################
+
+locals {
+  sm_guid   = var.existing_sm_instance_guid == null ? module.secrets_manager[0].secrets_manager_guid : var.existing_sm_instance_guid
+  sm_region = var.existing_sm_instance_region == null ? var.region : var.existing_sm_instance_region
+}
+
+
+########################################################################################################################
+# Resource Group
+########################################################################################################################
+
+module "resource_group" {
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.1.6"
+  # if an existing resource group is not set (null) create a new one using prefix
+  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
+  existing_resource_group_name = var.resource_group
+}
+
+########################################################################################################################
+# Secrets Manager resources
+########################################################################################################################
+
+#Create a new SM instance if not using an existing one
+module "secrets_manager" {
+  source               = "terraform-ibm-modules/secrets-manager/ibm"
+  version              = "1.17.8"
+  count                = var.existing_sm_instance_guid == null ? 1 : 0
+  resource_group_id    = module.resource_group.resource_group_id
+  region               = local.sm_region
+  secrets_manager_name = "${var.prefix}-secrets-manager"
+  allowed_network      = "public-and-private"
+  sm_service_plan      = "trial"
+}
+
+# Create a secret group to place the certificate in
+module "secrets_manager_group" {
+  source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
+  version                  = "1.2.2"
+  region                   = local.sm_region
+  secrets_manager_guid     = local.sm_guid
+  secret_group_name        = "${var.prefix}-certs"
+  secret_group_description = "A secret group to store private certs"
+  providers = {
+    ibm = ibm.ibm-sm
+  }
+}
+
+module "secrets_manager_arbitrary_secret" {
+  source                  = "terraform-ibm-modules/secrets-manager-secret/ibm"
+  version                 = "1.3.2"
+  region                  = local.sm_region
+  secrets_manager_guid    = local.sm_guid
+  secret_group_id         = module.secrets_manager_group.secret_group_id
+  secret_name             = "${var.prefix}-enterprise-arbitrary-secret"
+  secret_description      = "Enterprise child IAM_APIKey"
+  secret_type             = "arbitrary"
+  secret_payload_password = module.enterprise.enterprise_accounts_iam_response[0].iam_apikey
 }
--- examples/advanced/variables.tf
+++ examples/advanced/variables.tf
@@ -7,10 +7,34 @@ variable "ibmcloud_api_key" {
 variable "prefix" {
   type        = string
   description = "Prefix to append to all resources created by this example"
-  default     = "enterprise"
+  default     = "tf-ibm"
 }
 
 variable "enterprise_name" {
   description = "Name of the enterprise account"
   type        = string
+}
+
+variable "existing_sm_instance_guid" {
+  type        = string
+  description = "Existing Secrets Manager GUID. The existing Secret Manager instance must have private certificate engine configured. If not provided an new instance will be provisioned."
+  default     = null
+}
+
+variable "existing_sm_instance_region" {
+  type        = string
+  description = "Required if value is passed into var.existing_sm_instance_guid"
+  default     = null
+}
+
+variable "region" {
+  type        = string
+  description = "Region to provision all resources created by this example."
+  default     = "us-south"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "Name of the resource group to use for this example. If not set, a resource group is created."
+  default     = null
 }
