e6da0d1da8fc2223e64faf7f07761dfdec3562d0
mukul-palit <mukul.palit@ibm.com>
2025-06-28 10:37:53 +0530

    update outputs

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -1,14 +1,15 @@
 output "enterprise_account_groups" {
   description = "List of account groups created in an Enterprise"
-  value       = module.enterprise.enterprise_account_groups
+  value = module.enterprise.enterprise_account_groups
 }
 
 output "enterprise_accounts" {
   description = "List of accounts created in an Enterprise"
-  value       = module.enterprise.enterprise_accounts
+  value = module.enterprise.enterprise_accounts
 }
 
 output "enterprise_accounts_iam_response" {
   description = "List of accounts with IAM details created in an Enterprise"
-  value       = module.enterprise.enterprise_accounts_iam_response
+  value = module.enterprise.enterprise_accounts_iam_response
+  sensitive = true
 }
