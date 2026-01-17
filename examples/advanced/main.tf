5f875a4eaddc2aba3ce86548de1601e03ce01ee8
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-17 16:58:49 +0000

    chore(deps): update terraform terraform-ibm-modules/secrets-manager-secret-group/ibm to v1.3.37 (#372)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -262,7 +262,7 @@ module "secrets_manager" {
 # Create a secret group to place the certificate in
 module "secrets_manager_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.3.36"
+  version                  = "1.3.37"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-certs"
