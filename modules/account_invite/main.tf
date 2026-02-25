f73ba611aa7baac2d40f76e5ceff0ef556157973
raagamounica <mounica.tellabati@ibm.com>
2026-02-25 18:45:37 +0530

    feat: add binary installation support for Waypoint runtime compatibility

--- modules/account_invite/main.tf
+++ modules/account_invite/main.tf
@@ -4,9 +4,22 @@ data "ibm_iam_trusted_profiles" "iam_trusted_profiles" {
   name       = var.existing_trusted_profile_name
 }
 
-resource "null_resource" "invite_user" {
-  for_each = { for idx, user_obj in var.users_to_invite : idx => user_obj }
+resource "terraform_data" "terraform_data" {
+  count = var.install_required_binaries ? 1 : 0
+  triggers_replace = {
+    user_email_trigger           = each.value.email
+    user_access_groups_trigger   = join(",", each.value.exisiting_access_groups)
+    trusted_profile_name_trigger = var.existing_trusted_profile_name    
+  }
+  provisioner "local-exec" {
+    command     = "${path.module}/scripts/install-binaries.sh ${local.binaries_path}"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
 
+resource "terraform_data" "invite_user" {
+  for_each = { for idx, user_obj in var.users_to_invite : idx => user_obj }
+  depends_on = [terraform_data.install_required_binaries]
   triggers = {
     user_email_trigger           = each.value.email
     user_access_groups_trigger   = join(",", each.value.exisiting_access_groups)
@@ -14,7 +27,7 @@ resource "null_resource" "invite_user" {
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/../scripts/invite_user.sh"
+    command = " ${path.module}/scripts/install-binaries.sh ${local.binaries_path}"
     environment = {
       API_KEY            = var.ibmcloud_api_key
       USER_EMAIL         = each.value.email
