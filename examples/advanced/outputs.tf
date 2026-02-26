e52904f0289c3e9936059e5fe350e48dbf512ebb
raagamounica <mounica.tellabati@ibm.com>
2026-02-26 22:20:46 +0530

    update output

--- examples/advanced/outputs.tf
+++ examples/advanced/outputs.tf
@@ -1,3 +1,7 @@
 ########################################################################################################################
 # Outputs
 ########################################################################################################################
+output "enterprise_account_iam_response" {
+  description = "The account with IAM details created in an Enterprise"
+  value       = module.enterprise.enterprise_accounts_iam_response
+}
