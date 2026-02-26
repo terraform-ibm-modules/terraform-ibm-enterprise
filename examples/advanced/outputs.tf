1189b0433f6f52ec39a72adee4d9a5ffcac051d3
raagamounica <mounica.tellabati@ibm.com>
2026-02-26 22:24:00 +0530

    update output-2

--- examples/advanced/outputs.tf
+++ examples/advanced/outputs.tf
@@ -4,4 +4,5 @@
 output "enterprise_account_iam_response" {
   description = "The account with IAM details created in an Enterprise"
   value       = module.enterprise.enterprise_accounts_iam_response
+  sensitive   = true
 }
